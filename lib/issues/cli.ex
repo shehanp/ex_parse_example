cad3150deb7d64cd70f16f9a6ada6350de2b07f2
Shehan Perera <shehan@vin65.com>
2017-10-04 17:43:27 -0700

    init create CLI module add EXdoc dep

---
+++ lib/issues/cli.ex
@@ -0,0 +1,28 @@
+defmodule Issues.CLI do
+	@default_count 4
+	
+	@moduledoc """
+	handle the command line parsing and the dispatch to the various functions that end up generating a table of the last _n_ issues in a github project
+	"""
+	
+	def run(command) do
+		parse_args(command)
+	end
+	
+	@doc """
+	
+	`command` - Will return a tuple of `{user, project, count}`, or `:help` 
+	
+	"""
+	def parse_args(command) do
+		parse = OptionParser.parse(argv, switches: [help: :boolean],
+										  aliases: [ h:   :help])
+		case parse do
+			{[help: true, _, _]} -> :help
+			{[_, [ user, project, count], _]} -> { user, project, count }
+			{_, [ user, project], _} -> { user, project, @default_count}
+			_ -> :help
+		end
+	end
+	
+end
