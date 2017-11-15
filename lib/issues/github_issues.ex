e6e30f21f2e42f6a5a555f72c67afeec697433ac
shehanp <shehanp@gmail.com>
2017-11-14 17:10:26 -0800

    indentation

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -11,7 +11,7 @@ defmodule Issues.GithubIssues do
 	@github_url Application.get_env(:issues, :github_url)
 	
 	def issues_url(user, project) do
-        "#{@github_url}/repos/#{user}/#{project}/issues"
+		"#{@github_url}/repos/#{user}/#{project}/issues"
 	end
 	
 	def handle_response({:ok, %{status_code: 200, body: body}}) do
